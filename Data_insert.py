2542b2bea711b0585c8337f719641094938743c8
xie4785812 <63435620+xie4785812@users.noreply.github.com>
2020-06-01 23:17:46 -0400

    Add files via upload

--- Data_insert.py
+++ Data_insert.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 from pymongo import UpdateOne
+from dateutil import parser
 import csv
 
 client = MongoClient(port=27017)
@@ -14,18 +15,19 @@ def add_stock_data(filename):
         nvda.readline()
         nvda_read = csv.reader(nvda, delimiter=',')
         for i in nvda_read:
-            date = str(i[0])
+            date = parser.parse(i[0])
             open_price = float(i[1])
             high = float(i[2])
             low = float(i[3])
             close = float(i[4])
             adj_close = float(i[5])
             volume = int(i[6])
-            ins = {"date": date, "open source": open_price, "high price": high, "low price": low, "close price": close,
+            ins = {"date": date, "open price": open_price, "high price": high, "low price": low, "close price": close,
                    "adjacent close price": adj_close, "volume": volume}
             col_stock.insert_one(ins)
 
 # add_stock_data('NVDA.csv')
+
 
 def add_gold_data(filename):
     col_stock = db.Gold
@@ -33,18 +35,18 @@ def add_gold_data(filename):
         gold.readline()
         gold_read = csv.reader(gold, delimiter=',')
         for i in gold_read:
-            date = str(i[0])
+            date = parser.parse(i[0])
             open_price = float(i[1])
             high = float(i[2])
             low = float(i[3])
             close = float(i[4])
             adj_close = float(i[5])
             volume = int(i[6])
-            ins = {"date": date, "open source": open_price, "high price": high, "low price": low, "close price": close,
+            ins = {"date": date, "open price": open_price, "high price": high, "low price": low, "close price": close,
                    "adjacent close price": adj_close, "volume": volume}
             col_stock.insert_one(ins)
 
-# add_gold_data('GOLD.csv')
+add_gold_data('GOLD.csv')
 
 def add_exchange_data(filename):
     name = filename[0:-4]
@@ -53,15 +55,16 @@ def add_exchange_data(filename):
         ex.readline()
         ex_read = csv.reader(ex, delimiter=',')
         for i in ex_read:
-            date = i[0]
+            date = parser.parse(i[0])
             open_price = i[1]
             high = i[2]
             low = i[3]
             close = i[4]
             adj_close = i[5]
             volume = i[6]
-            ins = {"date": date, "open source": open_price, "high price": high, "low price": low, "close price": close,
+            ins = {"date": date, "open price": open_price, "high price": high, "low price": low, "close price": close,
                    "adjacent close price": adj_close, "volume": volume}
             col_stock.insert_one(ins)
 
 add_exchange_data('CNY=X.csv')
+
