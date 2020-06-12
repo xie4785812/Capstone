58171254d5b2762beb006c4478a3871d9dd74ecf
xie4785812 <63435620+xie4785812@users.noreply.github.com>
2020-06-12 00:17:53 -0400

    Add files via upload

--- TickerTape.py
+++ TickerTape.py
@@ -4,22 +4,75 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import threading as thd
 import time
+from pymongo import MongoClient
+from pymongo import UpdateOne
+import datetime
+import threading
+import sys
 
-def crawl_price():
-    url = 'https://finance.yahoo.com/quote/NVDA?p=NVDA&.tsrc=fin-srch'
-
+def crawl_price(url):
     try:
         page = urlopen(url)
+        soup = bs4.BeautifulSoup(page, 'html.parser')
     except:
         print('Error opening the URL')
+        pass
 
-    soup = bs4.BeautifulSoup(page, 'html.parser')
+
 
     result = soup.find('div', {'class': 'My(6px) Pos(r) smartphone_Mt(6px)'}).find('span').text
 
     return result
 
+def relax(start_time):
+    while True:
+        now_time = datetime.datetime.now()
+        if now_time >= start_time:
+            print('start crawl')
+            break
+        print('not now')
+        time.sleep(1)
 
-while True:
-    print(crawl_price())
-    time.sleep(5)
+def start_run(end_time, url,collection):
+    while True:
+        try:
+
+            result = crawl_price(url)
+
+            now_time = datetime.datetime.now()
+
+            ins = {"time":now_time, 'price': result}
+            collection.insert_one(ins)
+            print(now_time, result)
+            if now_time >= end_time:
+                print('end crawl')
+                break
+            time.sleep(5)
+        except:
+            pass
+
+client = MongoClient(port=27017)
+print('Connect MongoDB Successful')
+db = client.StockAnalyze
+nvda = db['nvda_daily']
+start_time = datetime.datetime(2020,6,11,9,30,00)
+end_time = datetime.datetime(2020,6,11,16,0,0)
+
+def begin_crawl(url, name):
+    collection = db[name]
+    relax(start_time)
+    start_run(end_time, url, collection)
+
+# begin_crawl('https://finance.yahoo.com/quote/NVDA?p=NVDA&.tsrc=fin-srch')
+
+if __name__ == '__main__':
+    data = {'AAPL':'https://finance.yahoo.com/quote/AAPL?p=AAPL&.tsrc=fin-srch',
+            'NVDA':'https://finance.yahoo.com/quote/NVDA?p=NVDA&.tsrc=fin-srch',
+            'TSLA':'https://finance.yahoo.com/quote/TSLA?p=TSLA&.tsrc=fin-srch',
+            'FB':'https://finance.yahoo.com/quote/FB?p=FB&.tsrc=fin-srch',
+            'MSFT':'https://finance.yahoo.com/quote/MSFT?p=MSFT&.tsrc=fin-srch'}
+    input = sys.argv[1]
+    web = data[input]
+    print(input, web)
+    begin_crawl(web, input)
+
---
+++ model_test.py
@@ -0,0 +1,106 @@
+from pymongo import MongoClient
+import numpy as np
+import sklearn
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from sklearn.linear_model import ElasticNet
+from sklearn import linear_model
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn import svm
+client = MongoClient(port=27017)
+print('Connect MongoDB Successful')
+db = client.StockAnalyze
+
+def collect_data(name):
+    msft = db[name]
+
+    msft_cursor = msft.find({})
+
+    msft_data = []
+    for i in msft_cursor:
+        msft_data.append(float(i['price']))
+
+    msft_data = np.array(msft_data).reshape(-1, 1)
+    train_x = msft_data[:-13]
+    train_y = msft_data[1:-12]
+    test_x = msft_data[-13:]
+    test_y = msft_data[-12:]
+
+    return train_x,train_y,test_x,test_y
+
+
+
+def svm_model_test(train_x,train_y,test_x,test_y):
+    clf = svm.SVR()
+    clf.fit(train_x, train_y)
+
+    result = []
+    tmp = test_x[0:1].reshape(-1, 1)
+    for i in range(12):
+        price = clf.predict(tmp.reshape(-1, 1))
+        result.append(price)
+        tmp = price
+
+    print(mean_squared_error(result, test_y))
+    plt.title('SVM (06/09/2020)')
+    plt.xlabel('last minute before closing ')
+    plt.ylabel('price')
+    plt.ticklabel_format(useOffset=False)
+    plt.xticks(np.arange(0,60,5))
+    time = [i for i in range(0,60,5)]
+    plt.plot(time, test_y, 'b', label = 'test')
+    plt.plot(time,result, 'r', label = 'prediction')
+    plt.legend()
+    plt.show()
+
+def etn_reg(train_x,train_y,test_x,test_y):
+    etn = ElasticNet()
+    etn.fit(train_x, train_y)
+
+    result = []
+    tmp = test_x[0:1].reshape(-1,1)
+    for i in range(12):
+        price = etn.predict(tmp.reshape(-1,1))
+        result.append(price)
+        tmp = price
+    print(mean_squared_error(result, test_y))
+    plt.title('elastic net regression (06/09/2020)')
+    plt.xlabel('last minute before closing ')
+    plt.ylabel('price')
+    time = [i for i in range(0, 60, 5)]
+    plt.plot(time, test_y, 'b')
+    plt.plot(time, result, 'r')
+    plt.show()
+
+def lasso_reg(train_x,train_y,test_x,test_y):
+    lasso = linear_model.Lasso(alpha=0.1)
+    lasso.fit(train_x,train_y)
+
+    result = []
+    tmp = test_x[0:1].reshape(-1, 1)
+    for i in range(12):
+        price = lasso.predict(tmp.reshape(-1, 1))
+        result.append(price)
+        tmp = price
+    print(mean_squared_error(result, test_y))
+    plt.title('lasso regression (06/09/2020)')
+    plt.xlabel('last minute before closing ')
+    plt.ylabel('price')
+    plt.xticks(np.arange(0,60,5))
+    time = [i for i in range(0, 60, 5)]
+    plt.plot(time, test_y, 'b', label = 'test')
+    plt.plot(time, result, 'r', label = 'prediction')
+    plt.legend()
+    plt.show()
+
+def LSTM(tarin_x, train__y, test_x, test_y):
+    
+
+if __name__ == '__main__':
+    train_x, train_y, test_x, test_y = collect_data('MSFT')
+    # etn_reg(train_x,train_y,test_x,test_y)
+    svm_model_test(train_x,train_y,test_x,test_y)
+    lasso_reg(train_x, train_y, test_x, test_y)
